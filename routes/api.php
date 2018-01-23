77895c10b08e365dd561755ca42e3ed51f4eea9d
Lucas Gabriel <90lucasgabriel@gmail.com>
2018-01-23 17:37:04 -0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,8 +24,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 	Route::group(['prefix' => 'company/{company_id}/', 'as' => 'company.'], function(){
 		Route::resource('branch', 				'Api\BranchController', 				['except'=>['create', 'edit', 'delete']]);
 		Route::resource('category', 			'Api\CategoryController', 			['except'=>['create', 'edit', 'delete']]);
-		Route::resource('product', 				'Api\ProductController', 				['except'=>['create', 'edit', 'delete']]);
 		
+		//Route::resource('product', 				'Api\ProductController', 				['except'=>['create', 'edit', 'delete']]);
+		Route::get('product/{id}',	['as'=>'company.product.show', 'uses'=>'Api\ProductController@show']);
+		Route::get('product', 			['as'=>'company.product', 'uses'=>'Api\ProductController@index']);
+		Route::post('product', 			['as'=>'company.product.store', 'uses'=>'Api\ProductController@store']);
+
 		Route::resource('menu', 					'Api\MenuController', 					['except'=>['create', 'edit', 'delete']]);
 		Route::post(		'menu/{menu_id}/branch', 						['as'=>'menu.branch.sync', 		'uses'=>'Api\MenuController@syncBranch']);
 		Route::post(		'menu/{menu_id}/product', 					['as'=>'menu.product.sync', 	'uses'=>'Api\MenuController@syncProduct']);
